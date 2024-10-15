df02e549a05f55b31ef4585441702c1573fbcf1a
Zi Huang <zih@usc.edu>
2024-10-15 14:26:46 -0700

    The noSQL database is already constructed.

--- Database/NoSQL/Models/Products.js
+++ Database/NoSQL/Models/Products.js
@@ -1,8 +1,102 @@
 const mongoose = require('mongoose');
 
-const productSchema = new mongoose.Schema({
-
+const variantSchema = new mongoose.Schema({
+    variantId: {
+       type: String,
+       required: true
+    },
+    sku: {type: String},
+    price: {type: Number},
+    compareAtPrice: {type: Number},
+    inventoryQuantity: {type: Number},
+    requiresShipping: {
+        type: Boolean,
+        default: true
+    },
+    weight: {type: Number},
+    weightUnit: {
+        type: String,
+        enum: ['kg', 'g', 'lb', 'oz'],
+        default: 'kg'
+    },
+    selectedOptions: [{
+        name: {type: String},
+        value: {type: String}
+    }],
+    createdAt: {
+        type: Date,
+        default: Date.now
+    },
+    updatedAt: {
+        type: Date,
+        default: Date.now
+    }
 });
 
+const imageSchema = new mongoose.Schema({
+    imageId: {
+       type: String,
+       required: true
+    },
+    src: {
+      type: String,
+      required: true
+    },
+    altText: {type: String}
+});
+
+
+const productSchema = new mongoose.Schema({
+    shopifyId: {
+        type: String,
+        required: true,
+        unique: true
+    },
+    title: {
+        type: String,
+        required: true
+    },
+    descriptionHTML: {
+       type: String,
+       required: true
+    },
+    handle: {
+       type: String, 
+       required: true
+    },
+    vendor: {
+       type: String,
+       required: true
+    },
+    productType: {
+        type: String,
+        enum: ['Clothing and Accessories', 'Electronics and Gadgets', 'Home and Garden', 'Beauty and Personal Care',
+               'Health and Wellness', 'Food and Beverages', 'Toys and Hobbies', 'Automative', 'Sports and Outdoor Gear',
+               'Office Supplies and Stationery', 'Jewelry and Accessories', 'Books and Media', 'Pet Supplies', 
+               'Travel and Luggage', 'Baby and Kids Products', 'Tools and Hardware'],
+        required: true,
+
+    },
+    tags: [String],
+    options: [{
+        id: String,
+        name: String,
+        values: [String]
+    }],
+    variants: [variantSchema],
+    images: [imageSchema],
+    createdAt: {
+        type: Date,
+        default: Date.now
+    },
+    updatedAt: {
+        type: Date,
+        default: Date.now,
+    },
+    publishedAt: {
+        type: Date,
+        default: Date.now
+    }
+});
 productsCollection = mongoose.model('Products', productSchema);
 module.exports = productsCollection;
